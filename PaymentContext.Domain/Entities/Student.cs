c7e271433b5b205d83432d828e5a4eac74487f63
Valdemir Pinheiro <pinheiroone@gmail.com>
2021-01-14 11:25:45 -0300

    testing entities and VOs

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -29,7 +29,7 @@ namespace PaymentContext.Domain.Entities
 
         public void AddSubscription(Subscription subscription)
         {
-
+            
             var hasSubscriptionActive = false;
             foreach(var sub in _subscriptions)
             {
@@ -40,7 +40,10 @@ namespace PaymentContext.Domain.Entities
             AddNotifications(new Contract()
                 .Requires()
                 .IsFalse(hasSubscriptionActive, "Student.Subscriptions", "Você já tem uma assinatura ativa")
+                .AreEquals(0, subscription.Payments.Count, "Student.Subscription.Payments", "Esta assinatura não possui pagamentos")
                 );
+
+
         }
     }
 }
---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        //Red, Green, Refactor
+        [TestMethod]
+        public void shouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void shouldReturnSucessWhenCNPJIsValid()
+        {
+            var doc = new Document("34110468000150", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void shouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("12356458965")]
+        [DataRow("89665774253")]
+        [DataRow("03269875642")]
+        public void shouldReturnSucessWhenCPFIsvalid(string cpf)
+        {
+            var doc = new Document("12356898565", EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
